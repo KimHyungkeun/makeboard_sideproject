a72d70d9ddb43f62414e0cb4b0418d260232f8a7
KimHyungkeun <hyungkeun.kim95@gmail.com>
2023-08-29 00:07:07 +0900

    230829 commit 1) BoardInfoWithId에서 @JsonInclude 삭제

--- src/main/java/com/sideproject/makeboard/dto/BoardInfoWithId.java
+++ src/main/java/com/sideproject/makeboard/dto/BoardInfoWithId.java
@@ -1,12 +1,10 @@
 package com.sideproject.makeboard.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 
 import java.util.Date;
 @Data
-@JsonInclude
 public class BoardInfoWithId {
     private String nickName;
     private String title;
