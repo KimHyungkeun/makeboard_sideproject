127f726ab3cbee24e6ac76c3d92c4b03a9a529df
KimHyungkeun <hyungkeun.kim95@gmail.com>
2023-09-04 21:45:21 +0900

    230904 commit 1) WebConfig 설정 : CORS 에러 방지를 위해 Web 설정 (진행중) - *.roco.moe, localhost를 허용 IP로 설정 - 포트는 모두 허용

--- src/main/java/com/sideproject/makeboard/WebConfig.java
+++ src/main/java/com/sideproject/makeboard/WebConfig.java
@@ -6,6 +6,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    // allowdOrigin : 허용 Protocal,IP,Port 설정
+    // allowedMehotd : 허용 REST method 설정
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
