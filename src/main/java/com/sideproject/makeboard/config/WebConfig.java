b5a4125f7482a961c37d87cc6f79d5e586141ced
KimHyungkeun <hyungkeun.kim95@gmail.com>
2023-09-04 23:29:12 +0900

    230904 commit 1) allowedOriginPattern 변경 => https 패턴 변경 및 port 여부 없애기

--- src/main/java/com/sideproject/makeboard/config/WebConfig.java
+++ src/main/java/com/sideproject/makeboard/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://*.roco.moe:*", "https://*.roco.moe:*","http://localhost:*","https://localhost:*")
+                .allowedOriginPatterns("http(s?)://*.roco.moe(:*)", "http(s?)://localhost(:*)")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
