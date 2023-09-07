8f9a6ff87c7b29835ee766402cb9f82c7488615d
KimHyungkeun <hyungkeun.kim95@gmail.com>
2023-09-07 21:33:29 +0900

    230907 commit 1) Swagger 문서 진행

--- src/main/java/com/sideproject/makeboard/controller/BoardController.java
+++ src/main/java/com/sideproject/makeboard/controller/BoardController.java
@@ -5,9 +5,12 @@ import com.sideproject.makeboard.dto.BoardInfoWithId;
 import com.sideproject.makeboard.dto.BoardInsertInfo;
 import com.sideproject.makeboard.dto.BoardUpdateInfo;
 import com.sideproject.makeboard.service.BoardService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +19,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Tag(name = "게시판 API 목록")
 @Slf4j
 @RestController
 @RequestMapping("/board")
@@ -27,7 +31,13 @@ public class BoardController {
     }
 
     // 게시판 목록 전체 불러오기
+
+
     @GetMapping("list")
+    @Operation(summary = "게시판 전체목록 불러오기")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "게시판 내용 모두 불러오기"),
+    })
     public @ResponseBody List<BoardInfo> getBoardInfo(
             @RequestParam(value = "page", required = false)Long page,
             @RequestParam(value = "listCnt", required = false) Long listCnt) {
@@ -39,6 +49,11 @@ public class BoardController {
 
     // 특정 게시판 불러오기
     @GetMapping("view")
+    @Operation(summary = "특정 게시글 불러오기")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "특정 게시글 불러오기"),
+            @ApiResponse(responseCode = "404", description = "게시글 없음"),
+    })
     public ResponseEntity<BoardInfoWithId> getBoardInfoWithId(@RequestParam(value = "id")Long id) {
 
         BoardInfoWithId boardInfoWithId = boardService.getBoardInfoWithId(id);
@@ -51,6 +66,11 @@ public class BoardController {
 
     // 게시판에 글 생성
     @PostMapping
+    @Operation(summary = "게시판에 글을 새로 등록")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "게시글 등록 성공"),
+            @ApiResponse(responseCode = "400", description = "requestBody 중 필요한 영역에 대해 기입하지 않음"),
+    })
     public ResponseEntity<?> setBoardInfo(@RequestBody BoardInsertInfo boardInsertInfo) {
 
         if (boardInsertInfo.getNickname() == null || boardInsertInfo.getNickname().isEmpty()) {
@@ -85,6 +105,12 @@ public class BoardController {
 
     // 게시판 내의 글을 수정
     @PutMapping
+    @Operation(summary = "게시글 내용을 수정하기")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "게시글 수정 성공"),
+            @ApiResponse(responseCode = "400", description = "requestBody에서 필요한 영역에 대해 기입하지 않음"),
+            @ApiResponse(responseCode = "404", description = "해당 게시글이 존재하지 않음"),
+    })
     public ResponseEntity<String> putBoardInfo(@RequestBody BoardUpdateInfo boardUpdateInfo) {
 
         if (boardUpdateInfo.getId() == null) {
@@ -117,6 +143,12 @@ public class BoardController {
 
     // 게시판 내의 글을 삭제
     @DeleteMapping
+    @Operation(summary = "게시글 삭제하기")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "게시글 삭제 성공"),
+            @ApiResponse(responseCode = "400", description = "삭제에 있어 필요한 id나 password가 미입력되거나 잘못 기입됨"),
+            @ApiResponse(responseCode = "404", description = "삭제하려는 글이 존재하지 않음"),
+    })
     public ResponseEntity<String> deleteBoardInfo(@RequestParam(value = "id") Long id,
                                                   @RequestParam(value = "password")String password) {
 
