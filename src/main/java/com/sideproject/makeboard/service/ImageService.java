5b2e5b019e8bcfc96d6976c5fd76425b61840e68
KimHyungkeun <hyungkeun.kim95@gmail.com>
2023-09-11 22:48:15 +0900

    230911 commit 1) S3에 Image 파일을 첨부하는 기능 구현 => 아직 미완성 => S3 업로드 기능 작동여부까지만 확인하였음

--- src/main/java/com/sideproject/makeboard/service/ImageService.java
+++ src/main/java/com/sideproject/makeboard/service/ImageService.java
@@ -1,12 +1,36 @@
 package com.sideproject.makeboard.service;
 
-import com.sideproject.makeboard.dto.ImageResponseInfo;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
 
 @Service
 public class ImageService {
 
-    public ImageResponseInfo setImage () {
-        return null;
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.s3.dir}")
+    private String dir;
+
+    private final AmazonS3Client amazonS3Client;
+    private ImageService (AmazonS3Client amazonS3Client) { this.amazonS3Client = amazonS3Client;}
+
+    public String setImage (MultipartFile multipartFile) throws IOException {
+        String s3FileName = dir + "/" + UUID.randomUUID() + "_" + multipartFile.getOriginalFilename();
+        long size = multipartFile.getSize();
+
+        ObjectMetadata objMeta = new ObjectMetadata();
+        objMeta.setContentType(multipartFile.getContentType());
+        objMeta.setContentLength(size);
+
+        amazonS3Client.putObject(bucket, s3FileName, multipartFile.getInputStream(), objMeta);
+
+        return amazonS3Client.getUrl(bucket, s3FileName).toString();
     }
 }
